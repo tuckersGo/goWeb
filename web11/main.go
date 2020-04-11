c032fb64738cad1b97182b95ee060bcdce0761ca
tucker <tucker@naver.com>
2020-04-10 18:07:16 -0700

    web10

---
+++ web11/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+type User struct {
+	Name  string
+	Email string
+	Age   int
+}
+
+func (u User) IsOld() bool {
+	return u.Age > 30
+}
+
+func main() {
+	user := User{Name: "tucker", Email: "tucker@naver.com", Age: 23}
+	user2 := User{Name: "aaa", Email: "aaa@gmail.com", Age: 40}
+	users := []User{user, user2}
+	tmpl, err := template.New("Tmpl1").ParseFiles("templates/tmpl1.tmpl", "templates/tmpl2.tmpl")
+	if err != nil {
+		panic(err)
+	}
+	tmpl.ExecuteTemplate(os.Stdout, "tmpl2.tmpl", users)
+}
